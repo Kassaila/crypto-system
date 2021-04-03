c98fad9a87f91188e8ea94b8495d9cad21fa4455
Kassaila <kasela.kassaila@gmail.com>
2021-04-03 16:00:20 +0300

    add registration pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ import preloading from '@js/modules/preloader';
 import headerMenu from '@js/modules/header-menu';
 import details from '@js/modules/details';
 
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
   preloading();
   headerMenu();
   details();
