6159f781fc5164ed08fb92d4f6873a0867c5dcf6
Kassaila <kasela.kassaila@gmail.com>
2021-04-18 22:26:22 +0300

    add page markets

--- assets/js/modules/details.js
+++ assets/js/modules/details.js
@@ -3,9 +3,9 @@ import Details from '@vendor/details';
 const detailsList = document.querySelectorAll('details');
 
 function init() {
-  if (detailsList.length === 0) return [];
+  if (detailsList.length === 0) return;
 
-  return detailsList.map((detail) => new Details(detail));
+  detailsList.forEach((detail) => new Details(detail));
 }
 
 export default init;
